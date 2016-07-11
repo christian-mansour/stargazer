d1f25c8a9744de6590edd7391fd47fc8d2b87df0
Christian Mansour <c.mansour@miami.edu>
2016-07-11 18:41:17 -0500

    stargazer completed

--- js/stargazer.js
+++ js/stargazer.js
@@ -1,5 +1,31 @@
+function displayImage(json){
+  var image =json.image.src;
+  $('#preview').html(`<div><img src="${image}"/></div>`);
+}
+
+
 function search(name) {
   var api = 'http://www.strudel.org.uk/lookUP/json/?name='+name;
-  // requires "jsonp" datatype.
-  
-}
+
+  $.ajax({
+    method: "GET",
+    url: api,
+    dataType: "jsonp",
+    jsonpCallback: "displayImage"
+  })
+
+};
+
+
+$(document).ready(function(){
+
+  $("#search").on("submit", function(event){
+    event.preventDefault();
+    var name = $('#search-name').val();
+    search(name);
+
+  });
+
+
+
+});
